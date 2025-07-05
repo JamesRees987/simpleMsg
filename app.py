4e77e62890e7c8ede7cf0a43ec6469447b6f4010
James <jrees183@gmail.com>
2025-07-05 11:25:44 +0100

    Form basic setup

--- app.py
+++ app.py
@@ -7,6 +7,13 @@ import os
 
 app = Flask(__name__)
 app.secret_key = os.environ["simpleMsgFlaskKey"]
+
+from flask_bootstrap import Bootstrap4
+from flask_wtf import CSRFProtect
+
+Bootstrap4(app)
+csrf = CSRFProtect(app)
+
 app.register_blueprint(route)
 
 if __name__ == '__main__':
--- route.py
+++ route.py
@@ -1,7 +1,13 @@
 from flask import Blueprint, render_template
+from forms import TestForm
 
 route = Blueprint('route', __name__)
 
-@route.route('/')
+@route.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template("home.html")
+    form = TestForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        print(name)
+    
+    return render_template("home.html", form=form)
